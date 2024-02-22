7421518d161ab8ee34f1426c22f0a69d68c41add
ameen121 <ameenusman121@gmail.com>
2024-02-23 02:56:40 +0500

    upload project

---
+++ data.js
@@ -0,0 +1,8 @@
+const data=
+    [
+        {name: 'ameen', email: 'ameenusman121@gmail.com'},
+        {name: 'uzair', email: 'uzair@gmail.com'},
+        {name: 'usman', email: 'usman@gmail.com'},
+    ];
+
+module.exports=data;
